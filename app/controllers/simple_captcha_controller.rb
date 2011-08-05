83c113e7e894f22c34a1df81d132f897c3968942
kkempin <kkempin@MacBook-Pro-Krzysztof.local>
2011-08-05 13:19:16 +0200

    changes in controller with streaming and missing send_file

--- app/controllers/simple_captcha_controller.rb
+++ app/controllers/simple_captcha_controller.rb
@@ -1,15 +1,17 @@
 class SimpleCaptchaController < ActionController::Metal
-  include ActionController::Streaming
+  # Changes: Streaming -> DataStreaming
+  include ActionController::DataStreaming
   include SimpleCaptcha::ImageHelpers
 
   # GET /simple_captcha
   def show
     unless params[:id].blank?
-      send_file(
-        generate_simple_captcha_image(params[:id]),
-        :type => 'image/jpeg',
-        :disposition => 'inline',
-        :filename => 'simple_captcha.jpg')
+      # looks like some problems with not existing send_file in Rails 3.1.0rc5
+      self.status = 200
+      self.content_type = "image/jpeg"
+      self.headers['Content-Disposition'] = 'inline; filename=simple_captcha.jpg' 
+      self.headers['Content-Transfer-Encoding'] = 'binary' 
+      self.response_body = File.open(generate_simple_captcha_image(params[:id]), "rb")
     else
       self.response_body = [404, {"Content-Type" => "text/html"}, ["Not Found"]]
     end
